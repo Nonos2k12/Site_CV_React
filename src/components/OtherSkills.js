9a46be36b98b0c29dde8b0573b41c43ac40b3e90
arnaud.jouanne <arnaud.jouanne89@gmail.com>
2021-10-08 14:53:38 +0200

    Creation of the Knowledges page components

---
+++ src/components/OtherSkills.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OtherSkills = () => {
+  return <div>autres compétences</div>;
+};
+
+export default OtherSkills;
---
+++ src/components/knowledges/Languages.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Languages = () => {
+  return <div>Langages</div>;
+};
+
+export default Languages;
