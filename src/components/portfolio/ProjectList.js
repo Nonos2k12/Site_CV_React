516346e3fcad5cb80dab27e7075e4600a9ffb9e8
arnaud.jouanne <arnaud.jouanne89@gmail.com>
2021-10-09 15:17:51 +0200

    Still working on the portfolio page + add the style of the page

--- src/components/portfolio/ProjectList.js
+++ src/components/portfolio/ProjectList.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
+import { portfolioData } from "../../data/portfolioData";
+import Project from "./Project";
 
-class ProjectList extends Component {
+export default class ProjectList extends Component {
+  state = {
+    projects: portfolioData,
+  };
+
   render() {
-    return <div>Projet</div>;
+    let { projects } = this.state;
+
+    return (
+      <div className="portfolioContent">
+        <ul className="radioDisplay"></ul>
+
+        <div className="projects">
+          {projects.map((item) => {
+            return <Project key={item.id} item={item} />;
+          })}
+        </div>
+      </div>
+    );
   }
 }
-
-export default ProjectList;
