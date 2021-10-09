7b73473b833fef7f883d4a43e9e5b20d5555b13c
arnaud.jouanne <arnaud.jouanne89@gmail.com>
2021-10-09 16:39:47 +0200

    Finish the portfolio page, add radio buttons and handleRadio function

--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
 
 export default class Project extends Component {
+  state = {
+    showInfo: false,
+  };
+
+  handleInfo = () => {
+    this.setState({
+      showInfo: !this.state.showInfo,
+    });
+  };
+
   render() {
     let { name, languagesIcons, source, info, picture } = this.props.item;
     return (
@@ -11,10 +21,36 @@ export default class Project extends Component {
           ))}
         </div>
         <h3>{name}</h3>
-        <img src={picture} alt="" />
-        <span className="infos">
+        <img src={picture} alt="" onClick={this.handleInfo} />
+        <span className="infos" onClick={this.handleInfo}>
           <i className="fas fa-plus-circle"></i>
         </span>
+
+        {this.state.showInfo && (
+          <div className="showInfos">
+            <div className="infosContent">
+              <div className="head">
+                <h2>{name}</h2>
+                <div className="sourceCode">
+                  <a
+                    href={source}
+                    rel="noopenner noreferrer"
+                    className="button"
+                    target="_blank"
+                  >
+                    Code Source
+                  </a>
+                </div>
+              </div>
+
+              <p className="text">{info}</p>
+
+              <div className="button return" onClick={this.handleInfo}>
+                Retourner sur la page
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
