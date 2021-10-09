7196db60acdfe5e7009e02ce035d1985a5148534
arnaud.jouanne <arnaud.jouanne89@gmail.com>
2021-10-09 17:13:45 +0200

    Start working & finish the contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,77 @@
 import React from "react";
 import Navigation from "../components/Navigation";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 const Contact = () => {
   return (
-    <div>
+    <div className="contact">
       <Navigation />
-      Hello depuis contact
+      <div className="contactContent">
+        <div className="header"></div>
+        <div className="contactBox">
+          <h1>Contactez moi</h1>
+          <ul>
+            <li>
+              <i className="fas fa-map-marker-alt"></i>
+              <span>Clichy</span>
+            </li>
+            <li>
+              <i className="fas fa-mobile-alt"></i>
+              <CopyToClipboard text="0650309966">
+                <span
+                  className="clickInput"
+                  onClick={() => {
+                    alert("Téléphone copié");
+                  }}
+                >
+                  06 50 30 99 66
+                </span>
+              </CopyToClipboard>
+            </li>
+            <li>
+              <i className="fas fa-envelope"></i>
+              <CopyToClipboard text="arnaud.jouanne89@gmail.com">
+                <span
+                  className="clickInput"
+                  onClick={() => {
+                    alert("Email copié");
+                  }}
+                >
+                  arnaud.jouanne89@gmail.com
+                </span>
+              </CopyToClipboard>
+            </li>
+          </ul>
+        </div>
+        <div className="socialNetwork">
+          <ul>
+            <a
+              href="https://www.linkedin.com/in/arnaud-jouanne/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h4>Linkedin</h4>
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a
+              href="https://github.com/Nonos2k12"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h4>Github</h4>
+              <i className="fab fa-github"></i>
+            </a>
+            <a
+              href="https://www.google.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h4>Twitter</h4>
+              <i className="fab fa-twitter"></i>
+            </a>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
