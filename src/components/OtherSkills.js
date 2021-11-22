b881c39dc61d9964c06f6a879936d8dbdcc66ca9
arnaud.jouanne <arnaud.jouanne89@gmail.com>
2021-11-22 17:31:36 +0100

    Change the name of the otherSkills section & add a third column to add software than I use

--- src/components/OtherSkills.js
+++ src/components/OtherSkills.js
@@ -3,17 +3,31 @@ import React from "react";
 const OtherSkills = () => {
   return (
     <div className="otherSkills">
-      <h3>Autres compétences</h3>
+      <h3>Logiciels et Soft-Skills</h3>
       <div className="list">
         <ul>
           <li>
+            <i className="fas fa-check-square"></i>Visual Studio Code
+          </li>
+          <li>
+            <i className="fas fa-check-square"></i>Postman
+          </li>
+          <li>
+            <i className="fas fa-check-square"></i>MySQL
+          </li>
+          <li>
+            <i className="fas fa-check-square"></i>SQL Server
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <i className="fas fa-check-square"></i>MongoDB Compass
+          </li>
+          <li>
+            <i className="fas fa-check-square"></i>Git
+          </li>
+          <li>
             <i className="fas fa-check-square"></i>Anglais courant
-          </li>
-          <li>
-            <i className="fas fa-check-square"></i>Github
-          </li>
-          <li>
-            <i className="fas fa-check-square"></i>Méthodes agiles
           </li>
           <li>
             <i className="fas fa-check-square"></i>Permis B
@@ -30,7 +44,7 @@ const OtherSkills = () => {
             <i className="fas fa-check-square"></i>Autonome
           </li>
           <li>
-            <i className="fas fa-check-square"></i>Force de proposition
+            <i className="fas fa-check-square"></i>Méthodes agiles
           </li>
         </ul>
       </div>
